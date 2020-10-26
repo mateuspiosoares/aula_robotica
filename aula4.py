010a4434fb0b4ff705982f817ca2c1a525005f60
Ubuntu <ubuntu@ip-172-31-86-213.ec2.internal>
2020-10-26 23:14:47 +0000

    segundo

--- aula4.py
+++ aula4.py
@@ -2,9 +2,18 @@ import rospy
 from std_msgs.msg import String
 
 
-rospy.init_node('aula4')
+rospy.init_node('aula4_1')
+
+
+mensagem = ''
+
+def recebe(mesagem_recebida):
+    global mensagem
+    mensagem = mesagem_recebida.data
+
 
 def timerCallBack(event):
+    print(mensagem)
     msg = String()
     msg.data = '2017006353'
     pub.publish(msg)
@@ -12,5 +21,6 @@ def timerCallBack(event):
 
 pub = rospy.Publisher('/topic1', String, queue_size=1)
 timer = rospy.Timer(rospy.Duration(0.1), timerCallBack)
+sub = rospy.Subscriber('/topic2', String, recebe)
 
 rospy.spin()
