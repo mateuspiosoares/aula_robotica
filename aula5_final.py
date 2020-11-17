8a4477e15571ab68e4901034fc9bb914ed2d3e61
Ubuntu <ubuntu@ip-172-31-85-234.ec2.internal>
2020-11-17 14:49:17 +0000

    -

--- aula5_final.py
+++ aula5_final.py
@@ -147,11 +147,11 @@ def timerCallBack(event):
             pub.publish(msg)
             state = 2
             print("State: ", state)
-          
+    '''      
     elif state == 2:
         if abs(error) > 1 or read > 0.5:
             state = 0
-
+    '''
     
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 odom_sub = rospy.Subscriber('/odom', Odometry, odomCallBack)
